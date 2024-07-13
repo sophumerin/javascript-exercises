7f49dcc0c5b2a81f6a1b3e479ed8bbb1ed7ea67f
sophumerin <79028412+sophumerin@users.noreply.github.com>
2024-07-13 11:07:46 -0400

    implement reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let reversedString = '';
+    for(let i = s.length - 1; i >= 0; i--){
+        reversedString += s.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
