c622f5599998d9bceac215516b1505db837d6477
sophumerin <79028412+sophumerin@users.noreply.github.com>
2024-07-13 11:24:38 -0400

    implement sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof(a) !== "number" || typeof(b) !== "number"){
+        return "ERROR";
+    }
+    if (a < 0 || b < 0){
+        return "ERROR";
+    }
+    return (a+b) * (Math.abs(b-a)+1)/2;
 };
 
 // Do not edit below this line
