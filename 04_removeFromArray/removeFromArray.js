78f8c551e67f12bce9a1c9cdded5176b7f4502a5
sophumerin <79028412+sophumerin@users.noreply.github.com>
2024-07-13 11:17:48 -0400

    implement removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for(let i = 0; i < toRemove.length; i++){
+        arr = arr.filter((elem) => elem !== toRemove[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
