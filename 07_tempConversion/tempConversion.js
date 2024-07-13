ca175b6480cb412e9d8f2817312ab8f5e8b1ab1e
sophumerin <79028412+sophumerin@users.noreply.github.com>
2024-07-13 11:31:54 -0400

    implement temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(10* (fahrenheit - 32) * 5 / 9) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(10* (celsius * 9 / 5 + 32)) / 10;
 };
 
 // Do not edit below this line
